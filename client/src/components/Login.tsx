39acf4965dc138d8a772eaca738266eb115d68a1
raoufbouk <zakib2316@gmail.com>
2024-12-29 23:44:14 +0100

    add login page and install notistack

---
+++ client/src/components/Login.tsx
@@ -0,0 +1,63 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPass] = useState("");
+  return (
+    <div className="flex justify-center items-center bg-black h-screen">
+      <div className="w-[500px] shadow-md  shadow-gray-500  rounded-md text-white">
+        <h1 className="text-center text-3xl">Login</h1>
+        <form
+          action=""
+          className="px-16 py-5"
+          onSubmit={(e) => {
+            e.preventDefault();
+          }}
+        >
+          <div>
+            <label className="block w-fit" htmlFor="email">
+              Email
+            </label>
+            <input
+              className="bg-black border rounded-md outline-none border-gray-500 p-1 text-lg px-2 my-3 w-full"
+              type="email"
+              id="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div>
+            <label className="block w-fit" htmlFor="pass">
+              Password
+            </label>
+            <input
+              className="bg-black border rounded-md outline-none border-gray-500 p-1 text-lg px-2 my-3 w-full"
+              type="password"
+              id="pass"
+              value={password}
+              onChange={(e) => setPass(e.target.value)}
+            />
+          </div>
+          <button
+            type="submit"
+            className="bg-gray-500 px-4 py-2 rounded-md mx-auto block w-fit"
+          >
+            Login
+          </button>
+          <p className="text-center block my-3">
+            Don&apos;t have an account?{" "}
+            <Link
+              to="/register"
+              className="underline hover:text-gray-500 transition-all"
+            >
+              Register
+            </Link>
+          </p>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,12 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { SnackbarProvider } from "notistack";
 import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <SnackbarProvider>
+        <App />
+      </SnackbarProvider>
     </BrowserRouter>
   </StrictMode>
 );
