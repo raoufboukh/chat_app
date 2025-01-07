2171eba00544e46e8362bbb0e1d52d7fcdef1323
raoufbouk <zakib2316@gmail.com>
2025-01-07 20:13:09 +0100

    remove unusable controller

--- server/routers/message.route.js
+++ server/routers/message.route.js
@@ -2,11 +2,8 @@ import express from "express";
 import {
   getMessages,
   sendMessage,
-  updateMessage,
-  deleteMessage,
   getUsers,
 } from "../controllers/message.controller.js";
-// import { Message } from "../models/message.model.js";
 import { protectRoute } from "../middelware/middelware.js";
 
 const messageRouter = express.Router();
