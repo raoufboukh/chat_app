0b6742c033c2e9151196fdc764a169da5ddbd84e
raoufbouk <zakib2316@gmail.com>
2025-01-13 22:54:54 +0100

    remove unused controllers

--- server/controllers/control.js
+++ server/controllers/control.js
@@ -4,46 +4,6 @@ import jwt from "jsonwebtoken";
 import cloudinary from "../lib/cloudinary.js";
 import { generateToken } from "../lib/utils.js";
 
-// export const getInformation = (req, res) => {
-//   information.find().then((result) => {
-//     res.send(result);
-//   });
-// };
-
-// export const createInformation = (req, res) => {
-//   information
-//     .create(req.body)
-//     .then((result) => res.send(result))
-//     .catch((err) => res.send(err));
-// };
-
-// export const updateInformation = async (req, res) => {
-//   try {
-//     const { id } = req.params;
-//     const update = await information.findByIdAndUpdate(id, req.body);
-//     if (update) {
-//       res.status(200).send(update);
-//     } else {
-//       res.status(404).send("No data found");
-//     }
-//   } catch (err) {
-//     console.log(err);
-//   }
-// };
-
-// export const deleteInformation = async (req, res) => {
-//   try {
-//     const { id } = req.params;
-//     const del = await information.findByIdAndDelete(id);
-//     if (del) {
-//       res.status(200).send("deleted successfully");
-//     } else {
-//       res.status(404).send("No data found");
-//     }
-//   } catch (err) {
-//     console.log(err);
-//   }
-// };
 
 export const register = async (req, res) => {
   const { name, email, password } = req.body;
