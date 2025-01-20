cbfaa718914fe7caacc23a9a3ffa1f7dee85e19a
raoufbouk <zakib2316@gmail.com>
2025-01-20 11:25:05 +0100

    add preview at settings page

--- client/src/pages/Settings.tsx
+++ client/src/pages/Settings.tsx
@@ -1,6 +1,11 @@
 import { themes } from "../constants";
 import { useThemeStore } from "../store/themeStore";
 
+const PREVIEW_MESSAGES = [
+  { id: 1, message: "Hey! How are you doing?", isSent: false },
+  { id: 2, message: "I'm doing great, thanks for asking", isSent: true },
+];
+
 const SettingsPage = () => {
   const { theme, setTheme } = useThemeStore();
   return (
@@ -36,6 +41,56 @@ const SettingsPage = () => {
                 </span>
               </button>
             ))}
+          </div>
+        </div>
+        <h3 className="text-lg font-semibold mb-3">Preview</h3>
+        <div className="rounded-xl border border-base-300 overflow-hidden bg-base-100 shadow-lg">
+          <div className="p-4 bg-base-200">
+            <div className="max-w-lg mx-auto">
+              <div className="bg-base-100 rounded-xl shadow-sm overflow-hidden">
+                <div className="px-4 py-3 border-b border-base-300 bg-base-100">
+                  <div className="flex items-center gap-3">
+                    <div className="w-8 h-8 bg-primary flex items-center justify-center text-primary-content font-medium">
+                      J
+                    </div>
+                    <div>
+                      <h3 className="font-medium text-sm">John Doe</h3>
+                      <p className="text-xs text-base-content/70">Online</p>
+                    </div>
+                  </div>
+                </div>
+                {/* chat messages */}
+                <div className="p-4 space-y-4 min-h-[200px] max-h-[200px] overflow-y-auto bg-base-100">
+                  {PREVIEW_MESSAGES.map((msg) => (
+                    <div
+                      key={msg.id}
+                      className={`flex ${
+                        msg.isSent ? "justify-end" : "justify-start"
+                      }`}
+                    >
+                      <div
+                        className={`max-w-[80%] rounded-xl p-3 shadow-sm  ${
+                          msg.isSent
+                            ? "bg-primary text-primary-content"
+                            : "bg-base-200"
+                        }`}
+                      >
+                        <p className="text-sm">{msg.message}</p>
+                        <p
+                          className={`text-[10px] mt-1.5 ${
+                            msg.isSent
+                              ? "text-primary-content/70"
+                              : "text-base-content"
+                          }`}
+                        >
+                          12:00 PM
+                        </p>
+                      </div>
+                    </div>
+                  ))}
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
