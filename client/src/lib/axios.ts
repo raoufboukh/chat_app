8a43693338c89b6e46fcc449a678febf1cfd3cd8
raoufbouk <zakib2316@gmail.com>
2025-01-24 23:52:48 +0100

    change url

--- client/src/lib/axios.ts
+++ client/src/lib/axios.ts
@@ -1,7 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL:
-    import.meta.env.MODE === "development" ? "http://localhost:5000" : "/",
+  baseURL: "https://chat-app-api-eosin.vercel.app",
   withCredentials: true,
 });
--- client/src/store/AuthStore.ts
+++ client/src/store/AuthStore.ts
@@ -5,8 +5,7 @@ import { axiosInstance } from "../lib/axios";
 import { enqueueSnackbar } from "notistack";
 import { Socket, io } from "socket.io-client";
 
-const BASE_URL =
-  import.meta.env.MODE === "development" ? "http://localhost:5000" : "/";
+const BASE_URL = "https://chat-app-api-eosin.vercel.app";
 
 interface User {
   _id: string; // Ajout de l'identifiant de Mongoose
