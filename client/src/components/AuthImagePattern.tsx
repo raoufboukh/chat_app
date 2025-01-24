990bb594a7f73d08ab1a0880264a90fe44417927
raoufbouk <zakib2316@gmail.com>
2025-01-24 22:53:53 +0100

    add some padding

--- client/src/components/AuthImagePattern.tsx
+++ client/src/components/AuthImagePattern.tsx
@@ -5,7 +5,7 @@ interface AuthImagePatternProps {
 
 const AuthImagePattern = ({ title, subtitle }: AuthImagePatternProps) => {
   return (
-    <div className="hidden lg:flex items-center justify-center bg-base-200 p-12">
+    <div className="hidden lg:flex items-center justify-center bg-base-200 p-12 pt-20">
       <div className="max-w-md text-center">
         <div className="grid grid-cols-3 gap-3 mb-8">
           {[...Array(9)].map((_, i) => (
