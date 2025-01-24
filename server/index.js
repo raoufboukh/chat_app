924e6045ea0600cd5e4c66a3dbfe8c90fbb9c8e0
raoufbouk <zakib2316@gmail.com>
2025-01-25 00:10:00 +0100

    delete path

--- server/index.js
+++ server/index.js
@@ -6,7 +6,6 @@ import router from "./routers/routers.js";
 import messageRouter from "./routers/message.route.js";
 import cookieParser from "cookie-parser";
 import { app, server } from "./lib/socket.js";
-import path from "path";
 
 dotenv.config();
 
@@ -24,14 +23,6 @@ app.use("/", router);
 app.use("/message", messageRouter);
 
 const PORT = process.env.PORT || 5000;
-const _dirname = path.resolve();
-if (process.env.NODE_ENV == "production") {
-  app.use(express.static(path.join(_dirname, "../client/dist")));
-
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(_dirname, "client", "dist", "index.html"));
-  });
-}
 
 mongoose.connect(process.env.CONNECTION_URL).then(() => {
   console.log("db connected");
