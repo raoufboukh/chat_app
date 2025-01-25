72b324458a51ce56e9ce610a6b65beeb74a2d935
raoufbouk <zakib2316@gmail.com>
2025-01-25 13:32:13 +0100

    test

--- client/src/store/AuthStore.ts
+++ client/src/store/AuthStore.ts
@@ -5,7 +5,7 @@ import { axiosInstance } from "../lib/axios";
 import { enqueueSnackbar } from "notistack";
 import { Socket, io } from "socket.io-client";
 
-const BASE_URL = "https://chat-app-api-eosin.vercel.app";
+const BASE_URL = "http://localhost:5000";
 
 interface User {
   _id: string; // Ajout de l'identifiant de Mongoose
